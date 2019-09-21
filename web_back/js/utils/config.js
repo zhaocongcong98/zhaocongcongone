48e60ce3e15c07564ec221447d1dbe2d7a00c433
zhaocongcong98 <1274908194@QQ.com>
2019-09-21 23:03:24 +0800

    第一次初始化

---
+++ web_back/js/utils/config.js
@@ -0,0 +1,12 @@
+// var BASE_URL = 'http://localhost:8000';
+// var URLIST = {
+//   user_login: BASE_URL + '/admin/login',
+//   user_logout: BASE_URL + '/admin/logout',
+//   user_info: BASE_URL + '/admin/getuser'
+// };
+var BASE_URL='http://localhost:8000';
+var URLIST={
+   user_login:BASE_URL+'/admin/login',
+   user_logout:BASE_URL+'/admin/logout',
+   user_info:BASE_URL+'/admin/getuser',
+}
